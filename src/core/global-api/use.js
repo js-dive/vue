96564bb82e11c7b5afe9450446fe20eaa7a62965
gogoend <gogoend@qq.com>
2022-06-04 11:21:46 +0800

    docs(use): 了解插件安装

--- src/core/global-api/use.js
+++ src/core/global-api/use.js
@@ -10,13 +10,15 @@ export function initUse (Vue: GlobalAPI) {
     }
 
     // additional parameters
+    // 执行Vue.use时传入的额外（第一个之外的）参数作为args
     const args = toArray(arguments, 1)
-    args.unshift(this)
+    args.unshift(this) // 此处this为Vue类
     if (typeof plugin.install === 'function') {
       plugin.install.apply(plugin, args)
     } else if (typeof plugin === 'function') {
       plugin.apply(null, args)
     }
+    // 记录一下插件已安装完成的插件
     installedPlugins.push(plugin)
     return this
   }
