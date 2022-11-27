ad139bd746b5ae31df20469622d135f808ddf460
gogoend <gogoend@qq.com>
2022-11-28 02:34:46 +0800

    docs(keep-alive): 了解keep-alive中对于LRU算法的使用

--- src/core/components/keep-alive.js
+++ src/core/components/keep-alive.js
@@ -21,6 +21,12 @@ function matches (pattern: string | RegExp | Array<string>, name: string): boole
   return false
 }
 
+/**
+ * 清理KeepAlive组件实例组件实例中，已缓存的、不再符合缓存条件的组件_vnode
+ * include/exclude 发生变化时调用
+ * @param {*} keepAliveInstance KeepAlive组件实例
+ * @param {*} filter 匹配函数
+ */
 function pruneCache (keepAliveInstance: any, filter: Function) {
   const { cache, keys, _vnode } = keepAliveInstance
   for (const key in cache) {
@@ -34,6 +40,13 @@ function pruneCache (keepAliveInstance: any, filter: Function) {
   }
 }
 
+/**
+ * 根据给定的组件vnode唯一标识及存储它的容器，来销毁组件，清理已缓存的组件vnode
+ * @param {*} cache 缓存映射 组件vnode唯一标识 -> 组件vnode的映射
+ * @param {*} key 组件vnode唯一标识
+ * @param {*} keys 所有相关组件vnode唯一标识数组
+ * @param {*} current 当前vnode
+ */
 function pruneCacheEntry (
   cache: VNodeCache,
   key: string,
@@ -66,6 +79,8 @@ export default {
   },
 
   destroyed () {
+    // keep-alive卸载时，清理所有缓存的组件vnode
+    // 此处可以用for...in循环，因为this.cache没有原型
     for (const key in this.cache) {
       pruneCacheEntry(this.cache, key, this.keys)
     }
@@ -86,6 +101,7 @@ export default {
     if (componentOptions) {
       // check pattern
       const name: ?string = getComponentName(componentOptions)
+      // 如果符合不缓存的条件，就直接返回当前vnode，不再进行其它处理
       if (name && (
         (this.include && !matches(this.include, name)) ||
         (this.exclude && matches(this.exclude, name))
@@ -99,15 +115,19 @@ export default {
         // so cid alone is not enough (#3269)
         ? componentOptions.Ctor.cid + (componentOptions.tag ? `::${componentOptions.tag}` : '')
         : vnode.key
+      // 如果渲染时，发现组件vnode已被缓存，则将本次渲染的组件vnode中的组件实例设置为缓存过的、相同类型的组件实例
+      // 同时根据LRU算法，把当前组件的key从原来的位置删除，并放到数组末端，使其不会被过早回收（如果设置了max）
       if (cache[key]) {
         vnode.componentInstance = cache[key].componentInstance
         // make current key freshest
         remove(keys, key)
         keys.push(key)
       } else {
+        // 否则，对产生的组件vnode进行缓存
         cache[key] = vnode
         keys.push(key)
         // prune oldest entry
+        // 根据LRU算法，删除最旧的、缓存过的组件vnode（如果设置了max）
         if (this.max && keys.length > parseInt(this.max)) {
           pruneCacheEntry(cache, keys[0], keys, this._vnode)
         }
